e90b936796b85a9a793230a04be1992173875c15
Hillary Muller <hillaryaward@gmail.com>
2022-09-26 16:53:00 -0500

    commit tree 4e6430b6e764aa15b7e47b45c100faa31642dfee

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :yes_clickbait
+
+    CLICKBAIT = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+    def yes_clickbait
+        if CLICKBAIT.none? { |pattern| pattern.match title }
+        errors.add(:title, "must be clickbait")
+        end
+    end
 end
